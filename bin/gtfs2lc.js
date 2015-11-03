35bff3fd3dbb5263b8d12c80ae6a70f4fb47da04
Sander Ballieu <sander.ballieu@ugent.be>
2015-11-03 17:18:26 +0100

    gtfs2lc command line interface

--- bin/gtfs2lc.js
+++ bin/gtfs2lc.js
@@ -1,49 +1,23 @@
 #!/usr/bin/env node
-/* Pieter Colpaert */
 
-/**
- * This script reads an unzipped GTFS archive and maps it to a stream of connections
- */
+var program = require('commander'),
+    Mapper = require('../lib/gtfs2lc.js'),
+    fs = require('fs');
 
-var fs = require('fs'),
-    N3 = require('n3'),
-    N3Util = N3.util,
-    program = require('commander'),
-    Mapper = require('./lib/gtfs2lc.js');
+console.error("GTFS to linked connections converter use --help to discover more functions");
 
-console.error("GTFS to Linked Connections convertor. Use --help to discover more options.");
+program
+    .version('0.1.0')
+    .option('-p, --path <s>', 'Path to sorted GTFS files')
+    .parse(process.argv);
 
-var die = function (msg) {
-  console.log(msg);
-  process.exit();
+if (!program.path) {
+    program.path = './'
 }
 
-//check whether the location of the zip has been set and zip exists
-var path = "",
-    version = "",
-    baseuri = "";
-if (process.argv[2]) {
-  path = process.argv[2];
-}else {
-  die("Give a path towards your gtfs feed as a first argument");
-}
+console.log(program.path);
 
-if (!fs.existsSync(path)) {
-  die(path + " not found");
-}
+var mapper = new Mapper();
+mapper.getStoreFromGTFSFiles([], function () {
 
-//get the feedname: the name of the zip file
-if (/(.*\/)?(.*?)\.zip/.exec(path)) {
-  var feedname = /(.*\/)?(.*?)\.zip/.exec(path)[2];
-} else {
-  die ("Not a zipfile: " + path);
-}
-
-var mapper = new Mapper({feedname: feedname});
-mapper.promiseConnectionsStream(fs.createReadStream(path)).then(function (stream) {
-  stream.on("data", function (data) {
-    console.log(data);
-  }).on("error", function (error) {
-    console.error(error);
-  });
 });
