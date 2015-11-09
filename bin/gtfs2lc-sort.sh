1a9c4d35572bc5abea5a520850b268a228778818
brechtvdv <br5cht@hotmail.com>
2015-11-09 15:32:07 +0100

    Fix #10 dos2unix

--- bin/gtfs2lc-sort.sh
+++ bin/gtfs2lc-sort.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 [[ $# == 1 ]] && {
   cd $1 && {
+    echo Converting newlines dos2unix;
+    sed 's/\r//' stop_times.txt > stop_times_unix.txt; mv stop_times_unix.txt stop_times.txt &
+    sed 's/\r//' trips.txt > trips_unix.txt; mv trips_unix.txt trips.txt &
+    sed 's/\r//' calendar.txt > calendar_unix.txt; mv calendar_unix.txt calendar.txt &
+    sed 's/\r//' calendar_dates.txt > calendar_dates_unix.txt; mv calendar_dates_unix.txt calendar_dates.txt &
     echo Sorting files in directory $1;
     { head -n 1 stop_times.txt ; tail -n +2 stop_times.txt | sort -t , -k 1,5n ; } > stop_times2.txt ; mv stop_times2.txt stop_times.txt &
     { head -n 1 trips.txt ; tail -n +2 trips.txt | sort -t , -k 3 ; } > trips2.txt ; mv trips2.txt trips.txt &
