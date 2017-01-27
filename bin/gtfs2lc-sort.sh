53a29f653a82a95755911f2e6fec7bc17876b565
Pieter Colpaert <pieter.colpaert@okfn.org>
2017-01-27 09:53:04 +0100

    Also remove unicode artifacts in GTFS

--- bin/gtfs2lc-sort.sh
+++ bin/gtfs2lc-sort.sh
@@ -8,6 +8,13 @@
       sed 's/\r//' calendar.txt > calendar_unix.txt; mv calendar_unix.txt calendar.txt &
       sed 's/\r//' calendar_dates.txt > calendar_dates_unix.txt; mv calendar_dates_unix.txt calendar_dates.txt ;
     } ;
+    echo Removing UTF-8 artifacts in directory $1;
+    {
+      sed '1s/^\xEF\xBB\xBF//' stop_times.txt > stop_times_unix.txt; mv stop_times_unix.txt stop_times.txt &
+      sed '1s/^\xEF\xBB\xBF//' trips.txt > trips_unix.txt; mv trips_unix.txt trips.txt &
+      sed '1s/^\xEF\xBB\xBF//' calendar.txt > calendar_unix.txt; mv calendar_unix.txt calendar.txt &
+      sed '1s/^\xEF\xBB\xBF//' calendar_dates.txt > calendar_dates_unix.txt; mv calendar_dates_unix.txt calendar_dates.txt ;
+    } ;
     echo Sorting files in directory $1;
     { head -n 1 stop_times.txt ; tail -n +2 stop_times.txt | sort -t , -k 1,5n ; } > stop_times2.txt ; mv stop_times2.txt stop_times.txt &
     { head -n 1 trips.txt ; tail -n +2 trips.txt | sort -t , -k 3 ; } > trips2.txt ; mv trips2.txt trips.txt &
