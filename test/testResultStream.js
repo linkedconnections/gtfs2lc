92abaf6a3c976d40c34b4014ffaf37e5805f2fdc
Julian Rojas <julian.rojas87@gmail.com>
2018-08-02 18:23:28 +0200

    Process De Lijn GTFS datasource and create Linked Connections grouped by the 'entiteiten' defined by De Lijn on their open data API.

--- test/testResultStream.js
+++ test/testResultStream.js
@@ -1,52 +1,56 @@
 const gtfs2lc = require('../lib/gtfs2lc.js');
 const assert = require('assert');
 const N3 = require('n3');
+const fs = require('fs');
 
-describe('Testing whether result contains certain objects (regression tests)', function () {
-  this.timeout(15000);
+describe('Testing whether result contains certain objects (regression tests)', function() {
+  this.timeout(240000);
   var lcstreamToArray = function (options) {
     if (!options)
       options = {};
-    return new Promise( (resolve, reject) => {
-      var mapper = new gtfs2lc.Connections(options);
-      mapper.resultStream('./test/sample-feed', (stream) => {
+    return new Promise(async (resolve, reject) => {
+      let mapper = new gtfs2lc.Connections('./test/sample-feed/', options);
+      let baseUris = JSON.parse(fs.readFileSync('baseUris-example.json', 'utf-8'));
+
+      let connectionSources = await mapper.getConnectionsByZones();
+      let linkedConnections = {};
+      let count = 0;
+
+      Object.keys(connectionSources).forEach(key => {
+        let stream = connectionSources[key];
         if (options.format && options.format === "rdf") {
-          stream = stream.pipe(new gtfs2lc.Connections2Triples({}));
-        } else if(options.format && options.format === 'jsonld') {
-          stream = stream.pipe(new gtfs2lc.Connections2JSONLD());
+          stream = stream.pipe(new gtfs2lc.Connections2Triples(baseUris));
+        } else if (options.format && options.format === 'jsonld') {
+          stream = stream.pipe(new gtfs2lc.Connections2JSONLD(baseUris));
         }
-        var connections = [];
-        stream.on('data', connection => {
-          connections.push(connection);
-        });
-        stream.on('error', (error) => {
-          reject(error);
-        });
-        stream.on('end', () => {
-          resolve(connections);
+
+        let conns = [];
+
+        stream.on('data', c => {
+          conns.push(c);
+        })
+        .on('error', err => {
+          reject(err);
+        })
+        .on('end', () => {
+          linkedConnections[key] = conns;
+          finish();
         });
       });
+
+      let finish = async () => {
+        count++;
+        if(count >= Object.keys(connectionSources).length) {
+          await mapper.close();
+          resolve(linkedConnections);
+        }
+      };
     });
   };
 
   it('Stream should contain certain things', async () => {
-    var connections = await lcstreamToArray();
-    assert.equal(connections[0]['arrivalStop'],'8895232');
-    assert.equal(connections[0]['headsign'],'Ostende');
-  });
-
-  it('JSON-LD Stream should contain Connections', async () => {
-    var triples = await lcstreamToArray({
-      format : 'jsonld'
-    });
-    console.log(triples[0]);
-    assert.equal(triples[0]['@type'],'Connection');
-  });
-
-  it('RDF Stream should contain Connections', async () => {
-    var triples = await lcstreamToArray({
-      format : 'rdf'
-    });
-    assert.equal(triples[0].object,'http://semweb.mmlab.be/ns/linkedconnections#Connection');
+    let connections = await lcstreamToArray({format: 'jsonld'});
+    assert.equal(connections['Antwerpen'][0]['arrivalStop'],'https://data.delijn.be/stops/107814');
+    assert.equal(connections['Antwerpen'][0]['direction'],'Puurs - Brussel');
   });
 });
