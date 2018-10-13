72ad3e2b628c947b2b831a1114894d47bca730b6
Pieter Colpaert <pieter.colpaert@ugent.be>
2018-10-13 12:00:35 +0200

    Fix #34

--- test/testResultStream.js
+++ test/testResultStream.js
@@ -31,8 +31,10 @@ describe('Testing whether result contains certain objects (regression tests)', f
 
   it('Stream should contain certain things', async () => {
     var connections = await lcstreamToArray();
-    assert.equal(connections[0]['arrivalStop'],'NANAA');
-    assert.equal(connections[0]['headsign'],'City');
+    assert.equal(connections[0]['arrivalStop'],'BEATTY_AIRPORT');
+    assert.equal(connections[0]['drop_off_type'], 1);
+    assert.equal(connections[0]['pickup_type'], 1);
+    assert.equal(connections[0]['headsign'],'Shuttle');
   });
 
   it('JSON-LD Stream should contain Connections', async () => {
