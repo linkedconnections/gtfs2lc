2b1cbd67c211123c00d3b7f093e60d2caa0e5e08
Julian Rojas <julian.rojas87@gmail.com>
2018-10-31 14:30:14 +0100

    Introducing Node.js clustering and fix for trip_id generation

--- bin/gtfs2lc.js
+++ bin/gtfs2lc.js
@@ -15,7 +15,9 @@ program
   .option('-f, --format <format>', 'Format of the output. Possibilities: csv, ntriples, turtle, json, jsonld (default: json), mongo (extended JSON format to be used with mongoimport) or mongold')
   .option('-s, --startDate <startDate>', 'startDate in YYYYMMDD format')
   .option('-e, --endDate <endDate>', 'endDate in YYYYMMDD format')
-  .option('-b, --baseUris <baseUris>', 'path to a file that describes the baseUris in json')
+  .option('-b, --baseUris <baseUris>', 'path to a file containing the baseUris templates in json format')
+  .option('-fb, --fragmentBy <fragmentBy>', 'column name in stop_times.txt that determines fragmentation')
+  .option('-fi, --fragmentIndex <fragmentIndex>', 'path to a file containing the index that determines fragmentation')
   .option('-S, --store <store>', 'store type: LevelStore (uses your harddisk - for if you run out of RAM) or MemStore (default)')
   .arguments('<path>', 'Path to sorted GTFS files')
   .action(function (path) {
@@ -28,73 +30,29 @@ if (!program.path) {
   process.exit();
 }
 
-var mapper = new gtfs2lc.Connections(program.path, {
-  startDate: program.startDate,
-  endDate: program.endDate,
-  store: program.store
-});
-
 var baseUris = null;
 if (program.baseUris) {
   baseUris = JSON.parse(fs.readFileSync(program.baseUris, 'utf-8'));
 }
 
-mapper.getConnectionsByZones().then(connectionSources => {
-  Object.keys(connectionSources).forEach(key => {
-    let stream = connectionSources[key];
-    if (!program.format || program.format === "json") {
-      stream.on('data', connection => {
-        console.log(JSON.stringify(connection));
-      });
-    } else if (program.format === 'mongo') {
-      stream.pipe(new MongoStream()).on('data', connection => {
-        console.log(JSON.stringify(connection));
-      });
-    } else if (program.format === 'csv') {
-      //print header
-      console.log('"id","departureStop","departureTime","arrivalStop","arrivalTime","trip","route","operationZone"');
-      var count = 0;
-      stream.on('data', connection => {
-        console.log(count + ',' + connection["departureStop"] + ',' + connection["departureTime"].toISOString() + ','
-          + connection["arrivalStop"] + ',' + connection["arrivalTime"].toISOString() + ',' + connection["trip"]["trip_id"] + ','
-          + connection["trip"]["route_id"] + ',' + connection["operationZone"]);
-        count++;
-      });
-    } else if (['jsonld', 'mongold'].indexOf(program.format) > -1) {
-      //convert triples stream to jsonld stream
-      stream = stream.pipe(new Connections2JSONLD(baseUris));
-      //prepare the output
-      if (program.format === 'mongold') {
-        //convert JSONLD Stream to MongoDB Stream
-        stream = stream.pipe(new MongoStream());
-      }
-      stream = stream.on('data', connection => {
-        console.log(JSON.stringify(connection));
-      });
-    } else if (['ntriples', 'turtle'].indexOf(program.format) > -1) {
-      stream = stream.pipe(new gtfs2lc.Connections2Triples(baseUris));
-      if (program.format === 'ntriples') {
-        stream = stream.pipe(new N3.StreamWriter({ format: 'N-Triples' }));
-      } else if (program.format === 'turtle') {
-        stream = stream.pipe(new N3.StreamWriter({
-          prefixes: {
-            lc: 'http://semweb.mmlab.be/ns/linkedconnections#',
-            gtfs: 'http://vocab.gtfs.org/terms#',
-            xsd: 'http://www.w3.org/2001/XMLSchema#',
-            schema: "http://schema.org/"
-          }
-        }));
-      }
+var fragmentIndex = null;
+if (program.fragmentIndex) {
+  fragmentIndex = new Map(JSON.parse(fs.readFileSync(program.fragmentIndex, 'utf8')));
+}
 
-      stream.on('data', triple => {
-        console.log(triple);
-      });
-    }
+var mapper = new gtfs2lc.Connections(program.path, {
+  format: program.format,
+  baseUris: baseUris,
+  startDate: program.startDate,
+  endDate: program.endDate,
+  store: program.store,
+  fragmentBy: program.fragmentBy,
+  fragmentIndex: fragmentIndex
+});
 
-    stream.on('end', () => {
-      cleanUp();
-    });
-  });
+mapper.map().then(files => {
+  console.error('Linked Connections created successfully here:');
+  console.error(files);
 }).catch(err => {
   console.error(err);
   process.exit();
